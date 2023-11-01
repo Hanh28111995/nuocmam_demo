803defaa29aeab503542204e2ff3c2a545473019
hanhT <gm.tranhuuhanh@gmail.com>
2023-11-01 13:58:11 -0700

    first init

---
+++ asset/js/index.js
@@ -0,0 +1,37 @@
+$(function ($) {
+  let url = window.location.href;
+  $('li a').each(function () {
+    if (this.href === url) {
+      $(this).closest('li').addClass('active');
+    }
+  });
+});
+
+$('.wrap_item').click(function (ev) {
+  document.location.href = "Products_detail.html";
+})
+$('.card-body .card-title').click(function (ev) {
+  document.location.href = "Products_detail.html";
+})
+
+$('.news-item-larger').click(function (ev) {
+  document.location.href = "News_detail.html";
+})
+$('.news-item-smaller').click(function (ev) {
+  document.location.href = "News_detail.html";
+})
+
+function rollUpInPost(ad, e) {
+  e = e || window.event;
+  e.preventDefault();
+  var element = document.getElementById(ad)
+  window.scrollTo({ top: element.offsetTop - 161, behavior: 'smooth' });
+}
+
+
+var example_link1 = 'https://www.instagram.com/p/CxPaWT4u2zD'
+var example_link2 = 'https://www.instagram.com/p/CxPaWT4u2zD'
+var example_link3 = 'https://www.instagram.com/p/CxPaWT4u2zD'
+var getImgSource = '/media/?size=l'
+
+
---
+++ asset/postContent/postContent.js
@@ -0,0 +1,22 @@
+export const post_content = [
+    {
+        main_title: "Delicious Fish sauce recipe for Banh Khot",
+        main_introduce:"Banh Khot is a specialty dish in the East of Vietnam and has been available in many cities. Wanting a Banh Khot dish to be good, besides having the right batter need to have the right kind of seasoned fish sauce. However, not everyone knows how to make a delicous fish sauce for these dipping pancakes. If at the weekend you are planning to win some heart with new dishes for your family, make these Banh Khot, and you should immediately pocket these "tips" on how to make the delicious fish sauce below of Nuoc Mam Hoang Gia.",
+        p1:
+        {
+            
+        }
+
+
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
