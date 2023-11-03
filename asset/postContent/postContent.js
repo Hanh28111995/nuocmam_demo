3c2a98f8764999388ad6534fc5e306899a644be9
hanhT <gm.tranhuuhanh@gmail.com>
2023-11-02 17:41:52 -0700

    first init

--- asset/postContent/postContent.js
+++ asset/postContent/postContent.js
@@ -1,22 +1,62 @@
 export const post_content = [
     {
         main_title: "Delicious Fish sauce recipe for Banh Khot",
-        main_introduce:"Banh Khot is a specialty dish in the East of Vietnam and has been available in many cities. Wanting a Banh Khot dish to be good, besides having the right batter need to have the right kind of seasoned fish sauce. However, not everyone knows how to make a delicous fish sauce for these dipping pancakes. If at the weekend you are planning to win some heart with new dishes for your family, make these Banh Khot, and you should immediately pocket these "tips" on how to make the delicious fish sauce below of Nuoc Mam Hoang Gia.",
-        p1:
-        {
-            
-        }
-
-
-
+        main_content_html: [
+            {
+                parent: ``,
+                child:
+                    `Banh Khot is a specialty dish in the East of Vietnam and has been available in many cities.
+                Wanting a Banh Khot dish to be good, besides
+                having the right batter need to have the right kind of seasoned fish sauce.However, not
+                everyone knows how to make a delicous fish sauce
+                for these dipping pancakes.If at the weekend you are planning to win some heart with new
+                    dishes for your family, make these Banh Khot,
+                and you should immediately pocket these "tips" on how to make the delicious fish sauce below
+                of Nuoc Mam Hoang Gia.</p >`
+                    +
+                    `
+                <div class="img_post">
+                            <img src="./asset/image/post_example/cach-lam-nuoc-mam-cham-banh-khot-ngon-me-ly-miri.com_.vn_.jpg (1).png" alt="">
+                            <span>Want Banh Khot to be good need to have a good fish sauce</span>
+                        </div>
+                `
+            },
+            {
+                parent: `<p class="header_text" id="title-1">Prepare the ingredients for the Fish Sauce</p>`,
+                child:
+                    `<p>To make a Banh Khot fish sauce that tasted exactly like in the East, you need to prepare the
+                            following ingredients:</p>
+                            <p>
+                            Delicious fish sauce: 5 – 6 coffee table spoon
+                            <br>
+                            Carrot: 1 piece
+                            <br>
+                            Garlic: 1 piece
+                            <br>
+                            Red chilli pepper: 2 – 3 &nbsp;
+                            <br>
+                            Lime: 2 – 3 according to the family’s taste
+                            <br>
+                            Rice vinegar: 100ml
+                            <br>
+                            Green papaya or ripe, depending on the right texture: 200gr
+                            <br>
+                            White radish: 1
+                            <br>
+                            Cool boiled water: 100ml
+                            <br>
+                            Other seasoning: MGS, salt,…
+                        </p>`
+                ,
+            },
+            {
+                parent: '',
+                child: '',
+            }
+        ],
+        tag: ['banh khot', 'fish sauce', 'nuoc mam'],
+        link: '',
+        type: ['Blog']
     },
-    {
 
-    },
-    {
-
-    },
-    {
-
-    }
 ]
